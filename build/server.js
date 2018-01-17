9b5d1ca218a12de263bf22218743585be7f3b14b
ssmith-wombatweb <ssmith@wombatweb.us>
2018-01-16 20:52:49 -0800

    Rebuild

--- build/server.js
+++ build/server.js
@@ -1,13 +1,40 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import logger from 'morgan';
-import fs from 'fs';
-import http from 'http';
-import https from 'https';
-import compression from 'compression';
+'use strict';
 
-var _dotenv$config$parsed = dotenv.config().parsed,
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _https = require('https');
+
+var _https2 = _interopRequireDefault(_https);
+
+var _compression = require('compression');
+
+var _compression2 = _interopRequireDefault(_compression);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _dotenv$config$parsed = _dotenv2.default.config().parsed,
     _dotenv$config$parsed2 = _dotenv$config$parsed.HTTP_PORT,
     HTTP_PORT = _dotenv$config$parsed2 === undefined ? 80 : _dotenv$config$parsed2,
     _dotenv$config$parsed3 = _dotenv$config$parsed.HTTPS_PORT,
@@ -18,34 +45,33 @@ var _dotenv$config$parsed = dotenv.config().parsed,
     PRIVATE_KEY_FILE = _dotenv$config$parsed.PRIVATE_KEY_FILE,
     CERTIFICATE_FILE = _dotenv$config$parsed.CERTIFICATE_FILE;
 
-
-var app = express();
+var app = (0, _express2.default)();
 
 if (STATUS !== undefined) {
-  app.use(logger(STATUS));
+  app.use((0, _morgan2.default)(STATUS));
 }
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, '../build/dist')));
+app.use((0, _compression2.default)());
+app.use(_express2.default.static(_path2.default.join(__dirname, '../build/dist')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build/dist', 'index.html'));
+  res.sendFile(_path2.default.join(__dirname, '../build/dist', 'index.html'));
 });
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build/dist', 'index.html'));
+  res.sendFile(_path2.default.join(__dirname, '../build/dist', 'index.html'));
 });
 
-http.createServer(function (req, res) {
+_http2.default.createServer(function (req, res) {
   var hostname = req.headers.host.match(/:/g) ? req.headers.host.slice(0, req.headers.host.indexOf(':')) : req.headers.host;
   var redirect = 'https://' + hostname + ':' + HTTPS_PORT + req.url;
   res.writeHead(301, { Location: redirect });
   res.end();
 }).listen(HTTP_PORT, HOST);
 
-https.createServer({
-  key: fs.readFileSync(PRIVATE_KEY_FILE),
-  cert: fs.readFileSync(CERTIFICATE_FILE)
+_https2.default.createServer({
+  key: _fs2.default.readFileSync(PRIVATE_KEY_FILE),
+  cert: _fs2.default.readFileSync(CERTIFICATE_FILE)
 }, app).listen(HTTPS_PORT, HOST, function () {
   console.log('Design Bright site running on ' + HOST + ':' + HTTPS_PORT + '.');
 });
